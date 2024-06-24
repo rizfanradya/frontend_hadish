1528a5a27bfcb58bedf107ea6ddb6c1638cb221c
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-06-24 22:46:23 +0700

    user dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { toUserDashboard } from "./utils/constant";
 import UserDashboard from "./pages/user/dashboard";
+import { ThemeProvider } from "@material-tailwind/react";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/user/dashboard/index.tsx
+++ src/pages/user/dashboard/index.tsx
@@ -1,12 +1,65 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react";
+import { IconButton, Textarea } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaSearch } from "react-icons/fa";
 
 export default function UserDashboard({ docTitle }: { docTitle: string }) {
+  const [textareaHeight, setTextareaHeight] = useState("56px");
+  const { register, watch } = useForm<{ search: string }>({
+    defaultValues: { search: "" },
+  });
+
   useEffect(() => {
     (async () => {
       document.title = docTitle;
     })();
   }, []);
 
-  return <div>HELLO WORLD</div>;
+  useEffect(() => {
+    const searchValue = watch("search");
+    const numberOfRows = Math.ceil(searchValue.length / 80);
+    const newHeight = Math.min(numberOfRows) * 20 + 56;
+    setTextareaHeight(`${newHeight}px`);
+  }, [watch("search")]);
+
+  return (
+    <div className="bg-[#202124] text-white h-screen flex flex-col gap-8 items-center justify-center">
+      <h1 className="text-6xl font-medium">Hadish</h1>
+      {/* {watch("search").length} */}
+
+      <div
+        className={`flex w-full max-w-2xl flex-row items-center gap-2 rounded-[99px] border py-2 px-4`}
+        style={{ maxHeight: textareaHeight }}
+      >
+        <Textarea
+          {...register("search")}
+          resize={false}
+          placeholder="Search Hadish..."
+          className="h-full !border-0 focus:border-transparent overflow-hidden text-white"
+          containerProps={{
+            className: "grid h-full",
+          }}
+          labelProps={{
+            className: "before:content-none after:content-none",
+          }}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
+
+        <IconButton
+          color="white"
+          variant="text"
+          className="rounded-full"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <FaSearch size={20} />
+        </IconButton>
+      </div>
+
+      {/* {textareaHeight} */}
+    </div>
+  );
 }
