add211c83b099b02e2380c90e26acc9470199073
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-06-25 13:09:46 +0700

    Update index.tsx

--- src/pages/user/dashboard/index.tsx
+++ src/pages/user/dashboard/index.tsx
@@ -1,10 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { IconButton, Textarea } from "@material-tailwind/react";
+import {
+  Button,
+  Collapse,
+  IconButton,
+  List,
+  ListItem,
+  Navbar,
+  Textarea,
+  Typography,
+} from "@material-tailwind/react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaSearch } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+import { HiBars3 } from "react-icons/hi2";
+import { ACCESS_TOKEN } from "../../../utils/constant";
 
 export default function UserDashboard({ docTitle }: { docTitle: string }) {
+  const [openNav, setOpenNav] = useState(false);
   const [charactersPerIncrement, setCharactersPerIncrement] = useState(75);
   const [textareaHeight, setTextareaHeight] = useState("56px");
   const [borderRadius, setBorderRadius] = useState("99px");
@@ -16,6 +29,10 @@ export default function UserDashboard({ docTitle }: { docTitle: string }) {
     (async () => {
       document.title = docTitle;
       updateCharactersPerIncrement();
+      window.addEventListener(
+        "resize",
+        () => window.innerWidth >= 960 && setOpenNav(false)
+      );
       window.addEventListener("resize", updateCharactersPerIncrement);
       return () => {
         window.removeEventListener("resize", updateCharactersPerIncrement);
@@ -38,7 +55,7 @@ export default function UserDashboard({ docTitle }: { docTitle: string }) {
     }
   }, [watch("search")]);
 
-  const updateCharactersPerIncrement = () => {
+  function updateCharactersPerIncrement() {
     const width = window.innerWidth;
     if (width < 640) {
       setCharactersPerIncrement(30);
@@ -47,41 +64,226 @@ export default function UserDashboard({ docTitle }: { docTitle: string }) {
     } else {
       setCharactersPerIncrement(75);
     }
-  };
+  }
 
   return (
-    <div className="bg-[#202124] text-white h-screen flex flex-col gap-8 items-center justify-center px-6">
-      <h1 className="text-6xl font-medium">Hadish</h1>
-
-      <div
-        className={`flex w-full max-w-2xl flex-row items-center gap-2 rounded-[${borderRadius}] border py-2 px-4 transition-all duration-300`}
-        style={{ height: textareaHeight }}
+    <div className="h-screen overflow-hidden bg-[#202124]">
+      <Navbar
+        className="max-w-screen-xl px-4 py-2 mx-auto"
+        color="transparent"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
       >
-        <Textarea
-          {...register("search")}
-          resize={false}
-          placeholder="Search Hadish..."
-          className={`!border-0 focus:border-transparent overflow-hidden text-white transition-all duration-300`}
-          containerProps={{
-            className: "grid h-full",
-          }}
-          labelProps={{
-            className: "before:content-none after:content-none",
-          }}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        />
+        <div className="flex items-center justify-between text-white">
+          <Typography
+            as="a"
+            href="/"
+            variant="h6"
+            className="mr-4 cursor-pointer py-1.5 lg:ml-2"
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            Material Tailwind
+          </Typography>
+          {ACCESS_TOKEN && (
+            <div className="hidden lg:block">
+              <List
+                className="p-0 mt-4 mb-6 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                <Typography
+                  as="a"
+                  href="#"
+                  variant="small"
+                  color="white"
+                  className="font-medium"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  <ListItem
+                    className="flex items-center gap-2 py-2 pr-4"
+                    placeholder={undefined}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                  >
+                    Home
+                  </ListItem>
+                </Typography>
+                <Typography
+                  as="a"
+                  href="#"
+                  variant="small"
+                  color="white"
+                  className="font-medium"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  <ListItem
+                    className="flex items-center gap-2 py-2 pr-4"
+                    placeholder={undefined}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                  >
+                    Contact Us
+                  </ListItem>
+                </Typography>
+              </List>
+            </div>
+          )}
+          <div className="hidden gap-2 lg:flex">
+            <Button
+              variant="text"
+              size="sm"
+              color="white"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              Log In
+            </Button>
+            <Button
+              variant="gradient"
+              size="sm"
+              color="blue"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              Sign In
+            </Button>
+          </div>
+          <IconButton
+            variant="text"
+            color="white"
+            className="lg:hidden"
+            onClick={() => setOpenNav(!openNav)}
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            {openNav ? (
+              <FaXmark className="w-6 h-6" strokeWidth={2} />
+            ) : (
+              <HiBars3 className="w-6 h-6" strokeWidth={2} />
+            )}
+          </IconButton>
+        </div>
 
-        <IconButton
-          color="white"
-          variant="text"
-          className="rounded-full"
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
+        <Collapse open={openNav}>
+          {ACCESS_TOKEN && (
+            <List
+              className="p-0 mt-4 mb-6 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <Typography
+                as="a"
+                href="#"
+                variant="small"
+                color="white"
+                className="font-medium"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                <ListItem
+                  className="flex items-center gap-2 py-2 pr-4"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Home
+                </ListItem>
+              </Typography>
+              <Typography
+                as="a"
+                href="#"
+                variant="small"
+                color="white"
+                className="font-medium"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                <ListItem
+                  className="flex items-center gap-2 py-2 pr-4"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Contact Us
+                </ListItem>
+              </Typography>
+            </List>
+          )}
+
+          <div className="flex items-center w-full gap-2 flex-nowrap lg:hidden">
+            <Button
+              variant="outlined"
+              size="sm"
+              color="white"
+              fullWidth
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              Log In
+            </Button>
+            <Button
+              variant="gradient"
+              size="sm"
+              color="blue"
+              fullWidth
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              Sign In
+            </Button>
+          </div>
+        </Collapse>
+      </Navbar>
+
+      <div className="bg-[#202124] h-full text-white flex flex-col gap-8 items-center justify-center px-6 -mt-40">
+        <h1 className="text-6xl font-medium">Hadish</h1>
+
+        <div
+          className={`flex w-full max-w-2xl flex-row items-center gap-2 border py-2 px-4 transition-all duration-300`}
+          style={{ height: textareaHeight, borderRadius }}
         >
-          <FaSearch size={20} />
-        </IconButton>
+          <Textarea
+            {...register("search")}
+            resize={false}
+            placeholder="Search Hadish..."
+            className={`!border-0 focus:border-transparent overflow-hidden text-white transition-all duration-300`}
+            containerProps={{
+              className: "grid h-full",
+            }}
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          />
+
+          <IconButton
+            color="white"
+            variant="text"
+            className="rounded-full"
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            <FaSearch size={20} />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
