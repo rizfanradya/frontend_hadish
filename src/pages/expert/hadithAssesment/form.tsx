fadfc90a93bdba8b4c95f1fdd4e02af6c562d285
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-07-09 23:17:05 +0700

    Refactor table column names for Hadith page

--- src/pages/expert/hadithAssesment/form.tsx
+++ src/pages/expert/hadithAssesment/form.tsx
@@ -1,17 +1,29 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import axiosInstance from "../../../utils/axiosInstance";
-import { DECODE_TOKEN } from "../../../utils/constant";
-import { Button } from "@material-tailwind/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  Typography,
+} from "@material-tailwind/react";
 
 type dataType = {
   id: number;
-  hadith: number;
+  hadith_id: number;
+  evaluation: number;
+  hadith_arab: string;
+  hadith_melayu: string;
+  explanation: string;
   type_hadith: number;
-  explanation: number;
 };
 
 export default function FormHadithAssesment({
@@ -25,64 +37,271 @@ export default function FormHadithAssesment({
   setGetData: Dispatch<SetStateAction<boolean>>;
   typeHadithData: any[];
 }) {
+  const [open, setOpen] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
-  const { register, watch } = useForm<dataType>({ defaultValues: data });
+  const { register, watch, reset } = useForm<dataType>({ defaultValues: data });
 
-  // useEffect(() => {
-  //   const timeoutId = setTimeout(async () => {
-  //     if (watch("type_hadith") !== data?.type_hadith) {
-  //       try {
-  //         setLoading(true);
-  //         await axiosInstance.put(`/hadith/${data?.id}`, {
-  //           created_by: data?.created_by,
-  //           updated_by: DECODE_TOKEN?.id,
-  //           type_hadith: watch("type_hadith"),
-  //           hadith: data?.hadith,
-  //           explanation: data?.explanation,
-  //         });
-  //         handleRefreshData();
-  //       } catch (error) {
-  //         Swal.fire({
-  //           icon: "error",
-  //           title: "Server Error 404",
-  //           allowOutsideClick: false,
-  //         });
-  //       }
-  //       setLoading(false);
-  //     }
-  //   }, 1000);
-  //   return () => clearTimeout(timeoutId);
-  // }, [watch("type_hadith")]);
+  async function onSubmit() {
+    try {
+      setLoading(true);
+      await axiosInstance.post(`/hadith_assesment`, {
+        hadith_id: data?.id,
+        evaluation_id: watch("type_hadith"),
+      });
+      handleRefreshData();
+      handleOpen();
+    } catch (error) {
+      handleOpen();
+      Swal.fire({
+        icon: "error",
+        title: "Server Error 404",
+        allowOutsideClick: false,
+      });
+    }
+    setLoading(false);
+  }
 
   function handleRefreshData() {
     setGetData(!getData);
   }
+  function handleOpen() {
+    setOpen(!open);
+    reset();
+  }
 
   return (
     <>
-      {loading ? (
-        <Button
+      <Button
+        variant="gradient"
+        size="sm"
+        color="blue"
+        onClick={handleOpen}
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
+        Evaluate
+      </Button>
+
+      <Dialog
+        open={open}
+        handler={handleOpen}
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+        size="xl"
+      >
+        <DialogHeader
           placeholder={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}
-          className="bg-gray-200"
-          loading={loading}
-          disabled
         >
-          {" "}
-        </Button>
-      ) : (
-        <select
-          {...register("type_hadith")}
-          className="border cursor-pointer w-min border-gray-300 text-xs text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          Hadish Assesment
+        </DialogHeader>
+
+        <DialogBody
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          className="flex flex-col gap-4 h-[calc(100vh-18rem)] overflow-y-scroll"
         >
-          {typeHadithData.map((doc) => (
-            <option value={doc.id} key={doc.id} className="text-sm">
-              {doc.type}
-            </option>
-          ))}
-        </select>
-      )}
+          <Card
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            color="blue-gray"
+            className="mt-6"
+          >
+            <CardHeader
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+              variant="gradient"
+              color="green"
+              className="p-2 w-max"
+            >
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+                className="text-xs font-semibold uppercase"
+              >
+                Hadish Arab
+              </Typography>
+            </CardHeader>
+
+            <CardBody
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+                dir="rtl"
+              >
+                {data?.hadith_arab}
+              </Typography>
+            </CardBody>
+          </Card>
+
+          <Card
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            color="blue-gray"
+            className="mt-6"
+          >
+            <CardHeader
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+              variant="gradient"
+              color="green"
+              className="p-2 w-max"
+            >
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+                className="text-xs font-semibold uppercase"
+              >
+                Hadish Melayu
+              </Typography>
+            </CardHeader>
+
+            <CardBody
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                {data?.hadith_melayu}
+              </Typography>
+            </CardBody>
+          </Card>
+
+          <Card
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            color="blue-gray"
+            className="mt-6"
+          >
+            <CardHeader
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+              variant="gradient"
+              color="green"
+              className="p-2 w-max"
+            >
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+                className="text-xs font-semibold uppercase"
+              >
+                Explanation
+              </Typography>
+            </CardHeader>
+
+            <CardBody
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                {data?.explanation}
+              </Typography>
+            </CardBody>
+          </Card>
+
+          <Card
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            color="blue-gray"
+            className="mt-6"
+          >
+            <CardHeader
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+              variant="gradient"
+              color="green"
+              className="p-2 w-max"
+            >
+              <Typography
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+                className="text-xs font-semibold uppercase"
+              >
+                Evaluation
+              </Typography>
+            </CardHeader>
+
+            <CardBody
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <select
+                {...register("type_hadith")}
+                className="bg-gray-50 border cursor-pointer border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option value={""}>-</option>
+                {typeHadithData.map((doc) => (
+                  <option value={doc.id} key={doc.id}>
+                    {doc.type}
+                  </option>
+                ))}
+              </select>
+            </CardBody>
+          </Card>
+        </DialogBody>
+
+        <DialogFooter
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpen}
+            loading={loading}
+            className="mr-1"
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="gradient"
+            color="green"
+            onClick={onSubmit}
+            loading={loading}
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            disabled={!watch("type_hadith")}
+          >
+            Save
+          </Button>
+        </DialogFooter>
+      </Dialog>
     </>
   );
 }
