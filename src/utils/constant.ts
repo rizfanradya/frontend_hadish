06b07ee5999ddd68e62ad70cbefbfb250e47be94
rizfanradya <rizfankusuma@gmail.com>
2024-07-10 04:40:36 +0000

    refactor: update dev and preview scripts in package.json

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,7 @@
 import { jwtDecode } from "jwt-decode";
 
-export const backendFastApi = "http://192.168.43.174:8000/api";
+export const backendFastApi = "http://89.116.20.146:8000/api";
+// export const backendFastApi = "http://192.168.43.174:8000/api";
 
 export const ACCESS_TOKEN_NAME = "hadish_access_token";
 export const ACCESS_TOKEN = localStorage.getItem(ACCESS_TOKEN_NAME);
@@ -9,7 +10,7 @@ export const DECODE_TOKEN: { exp: number; id: string } | undefined =
   ACCESS_TOKEN ? jwtDecode(ACCESS_TOKEN) : undefined;
 
 // role
-export const roleSuperAdministrator = "SUPER ADMINISTRATOR";
+export const roleSuperAdministrator = "SUPER ADMIN";
 export const roleAdmin = "ADMIN";
 export const roleExpert = "EXPERT";
 export const roleUser = "USER";
