91381058a60182929f6ac98ea44baf217b77524d
rizfanradya <rizfankusuma@gmail.com>
2024-07-23 23:18:24 +0000

    refactor: Remove unnecessary option from role select in user form

--- src/pages/user/form.tsx
+++ src/pages/user/form.tsx
@@ -358,7 +358,6 @@ export default function FormUser({
               {...register("role")}
               className="bg-gray-50 border cursor-pointer border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              {mode === "add" && <option>-</option>}
               {roleData.map((doc) => (
                 <option value={doc.id} key={doc.id}>
                   {doc.role}
